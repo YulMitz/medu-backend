0eb4397f548a17d4fb59f186a6906395bad39a2e
shiloh <shilohlee98@gmail.com>
2024-12-30 15:24:07 +0800

    fix access token unit test

--- src/__tests__/userService.test.js
+++ src/__tests__/userService.test.js
@@ -244,7 +244,7 @@ describe('User Service', () => {
                 expect(jwt.sign).toHaveBeenCalledWith(
                     { userId },
                     process.env.SECRET_KEY,
-                    { expiresIn: '1m' }
+                    { expiresIn: '1h' }
                 );
             });
 
